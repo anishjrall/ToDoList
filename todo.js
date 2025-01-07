dd93ee5128eb4a69ce20efef0ee3a24a17b492f7
anishjrall <79248886+anishjrall@users.noreply.github.com>
2025-01-07 14:36:48 +0530

    todo.js

--- todo.js
+++ todo.js
@@ -1,14 +1,56 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>To-Do List</title>
-    <link rel="stylesheet" href="todo.css">
-</head>
-<body>
-    <input class="todo" onkeydown="keydown(event)" placeholder="Enter to-do">
-    <input class="date" type="date">
-    <button class="add" onclick="inputdata()">Add</button>
-    <div class="div"></div>
-    <script src="todo.js"></script>
-</body>
-</html>
+function keydown(event) {
+    if (event.key === "Enter") {
+        inputdata();
+    }
+}
+
+const todo = [
+    { name: "Study", date: "2025-01-05" },
+    { name: "Will Go For Trip", date: "2025-02-23" }
+];
+
+console.log(todo);
+printdata();
+
+function printdata() {
+    let out = "";
+    for (let i = todo.length - 1; i >= 0; i--) {
+        const { name, date } = todo[i];
+        let elem = `
+        <div class="name">${name}</div>
+        <div class="date">${date}</div>
+        <div>
+            <button class="button" onclick="removeItem(${i})">Delete</button>
+        </div>
+        `;
+        out += elem;
+    }
+    const container = document.querySelector('.div');
+    if (container) {
+        container.innerHTML = out;
+    }
+}
+
+function removeItem(index) {
+    todo.splice(index, 1);
+    printdata();
+}
+
+function inputdata() {
+    const nameInput = document.querySelector('input.todo');
+    const dateInput = document.querySelector('.date');
+    const name = nameInput.value.trim();
+    const date = dateInput.value;
+
+    if (name) {
+        if (date) {
+            todo.push({ name, date });
+        } else {
+            const newDate = new Date().toISOString().split('T')[0];
+            todo.push({ name, date: newDate });
+        }
+        printdata();
+        nameInput.value = '';
+        dateInput.value = '';
+    }
+}
